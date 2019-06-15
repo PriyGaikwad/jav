5175abddc921b20f5c4184841eedf489ce4210f1
Vasant <priyankagaikwad870@gmail.com>
2019-06-15 15:26:54 +0530

    commit

---
+++ src/com/model/A.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("kk");
+		System.out.println("hh");
+	}
+
+}
